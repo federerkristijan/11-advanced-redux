e1215b668b14585bbb03fccfee5146c2dbeaa3c1
federerkristijan <federer.kristijan@gmail.com>
2022-09-07 21:00:11 +0200

    app.js - api call fetch implemented

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -6,6 +7,15 @@ import Products from './components/Shop/Products';
 
 function App() {
   const showCard = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart)
+
+  useEffect(() => {
+    fetch('https://udemy-react-21466-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    });
+  }, [cart]);
+
 
   return (
     <Layout>
