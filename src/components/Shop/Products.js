45765dc17591dff30f9a80fffa4aeb34c6c397e0
federerkristijan <federer.kristijan@gmail.com>
2022-09-07 15:14:08 +0200

    ProductItem.js - addToCatHandler finished and implemented

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -22,9 +22,11 @@ const Products = (props) => {
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
+        {/* key vs. id -> with id we forward product.id data to the ProductIttem.js, whereas key is needed for mapping  */}
         {dummy_products.map((product) => (
           <ProductItem
             key={product.id}
+            id={product.id}
             title={product.title}
             price={product.price}
             description={product.description}
